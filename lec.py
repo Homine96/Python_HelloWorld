f8508e44f20975a6900cc56806ba626f6378c00c
Pavel <Shestakov1996@yandex.ru>
2022-12-11 09:33:45 +0300

    Python

---
+++ lec.py
@@ -0,0 +1,63 @@
+print ('hello world')
+
+# value=None
+# # print(type(value))
+# a = 123
+# b = 1.23
+# # print (type(a))
+# # print(type(b))
+# value=12345
+# # print(type(value))
+
+# s = "hello 'world'"
+# # print (s)
+
+# print(a, ' - ' ,b,' - ',s)
+# print('{1} - {2} - {0}'.format(a,b,s))
+# print(f'{a} - {b} - {s}')
+
+# f = True
+# print(f)
+
+# list = ['1','2','3']
+# print (list)
+
+# print ('ВВедите а')
+# a = int(input())
+# print ('ВВедите b')
+# b = int(input())
+# print (a,b,' ', a+b)
+# print('{} {}'.format(a,b))
+# print (f'{a} {b}')
+
+
+# a = 1.312321242414
+# b = 3
+# c = round(a * b, 5)
+# print(c)
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+#     print (a)
+# else:
+#     print(b)    
+
+
+# elif
+
+# original = 23
+# inverted = 0
+# while original !=0:
+#     inverted = inverted*10 + (original%10)
+#     original//=10
+# else:
+#     print('Пожалуй')
+#     print('Хватит')
+# print(inverted)        
+
+
+# for i in 'range(1,10,2)':
+#     print(i)
+
+# help(int)
